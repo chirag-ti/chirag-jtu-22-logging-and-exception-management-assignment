9b3af9bcb1c00c9bc3fa017f1b9e673c9de9a914
chirag-ti <chirag.mittal@trilogy.com>
2022-12-13 17:46:55 +0000

    Added logs and exceptions

--- fast_api_als/routers/lead_conversion.py
+++ fast_api_als/routers/lead_conversion.py
@@ -37,6 +37,7 @@ def get_quicksight_data(lead_uuid, item):
         "3pl": item.get('3pl', 'unknown'),
         "oem_responded": 1
     }
+    logging.info("Created the lead converted data for dumping into S3 for lead_uuid:{lead_uuid}")
     return data, f"{item['make']}/1_{int(time.time())}_{lead_uuid}"
 
 
@@ -47,7 +48,8 @@ async def submit(file: Request, token: str = Depends(get_token)):
 
     if 'lead_uuid' not in body or 'converted' not in body:
         # throw proper HTTPException
-        pass
+        raise HTTPException(status_code=404 , detail="lead_uuid or converted not available in body")
+        return
         
     lead_uuid = body['lead_uuid']
     converted = body['converted']
@@ -55,7 +57,8 @@ async def submit(file: Request, token: str = Depends(get_token)):
     oem, role = get_user_role(token)
     if role != "OEM":
         # throw proper HTTPException
-        pass
+        raise HTTPException(status_code=404 , detail="Invalid Role")
+        return
 
     is_updated, item = db_helper_session.update_lead_conversion(lead_uuid, oem, converted)
     if is_updated:
@@ -67,4 +70,5 @@ async def submit(file: Request, token: str = Depends(get_token)):
         }
     else:
         # throw proper HTTPException
-        pass
+        raise HTTPException(status_code=404 , detail="Failed to update lead conversion status")
+        return
